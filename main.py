57813c359ce7a815cb852a2de0d2b370bcb87b44
Neptune <52249849+Neptune-IT@users.noreply.github.com>
2022-05-18 22:36:44 +0200

    update [1.1.0] : changing structure

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
 #!/usr/bin/python3
-from sql.sql_manager import sql_manager
-import json
-
-manager = sql_manager()
-
-def main():
-    cursor = manager.connect_db().cursor()
-    cursor.execute("SELECT * FROM %s" % (manager.target_table))
-
-    if json.load(open("settings.json"))["settings"]["result_display_method"] == "file":
-        file = open("result.txt", "w")
-        file.write("-==== {} TABLE BUMP START ====-\n\n".format(manager.target_table.upper()))
-
-        for data in cursor:
-            file.write(str(data))
-            file.write("\n")
-        file.write("\nProcess finished with exit code 0")
-        file.close()
-        print("Data has been successfully written in the file 'result.txt'")
-    elif json.load(open("settings.json"))["settings"]["result_display_method"] == "console":
-        print("-==== {} TABLE BUMP START ====-\n".format(manager.target_table.upper()))
-        
-        for data in cursor:
-            print(str(data))
-    else: print("Wrong display method given.")
+from src.exctractor import extract_data
 
 if __name__ == "__main__":
-    main()
+    extract_data()
