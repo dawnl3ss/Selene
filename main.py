1aed0e20d6ba2eda8e5f9f3cad614fbe43f3e0d1
4m4Sec <neptune.dev.cybsec@gmail.com>
2023-09-18 21:30:04 +0200

    update [2.1.0] : change ascii art

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from src.style.ascii import get_ascii
 
 if __name__ == "__main__":
     parse_arguments()
+    print("☾ Starting Selene dumper...")
     print(get_ascii())
+    print("  Best database dumper. Written by Dawnl3ss. This tool is only design for educationnal and ethical purpose. I (dawnless) am not responsible for your usage.")
